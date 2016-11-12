7c14bff0b1fd1e64482b6c755a8c24602c4d4ead
Asourcious <barronpm@gmail.com>
2016-11-12 13:33:21 -0500

    Added basic BootLoader

--- src/main/java/org/asourcious/plusbot/BootLoader.java
+++ src/main/java/org/asourcious/plusbot/BootLoader.java
@@ -1,7 +1,36 @@
 package org.asourcious.plusbot;
 
-public class BootLoader {
-    public static void main(String[] args) {
+import net.dv8tion.jda.core.utils.SimpleLog;
 
+public class BootLoader {
+
+    private static PlusBot instance;
+    private static final SimpleLog LOG = SimpleLog.getLog("BootLoader");
+
+    private BootLoader() {}
+
+    public static void main(String[] args) {
+        createInstance();
+    }
+
+    public static void restart() {
+        LOG.info("Restarting...");
+        instance.shutdown(false);
+        createInstance();
+    }
+
+    public static void shutdown() {
+        LOG.info("Shutting down...");
+        instance.shutdown(true);
+    }
+
+    private static void createInstance() {
+        LOG.info("Creating instance...");
+        try {
+            instance = new PlusBot();
+        } catch (Exception ex) {
+            LOG.log(ex);
+            System.exit(1);
+        }
     }
 }
---
+++ src/main/java/org/asourcious/plusbot/PlusBot.java
@@ -0,0 +1,8 @@
+package org.asourcious.plusbot;
+
+public class PlusBot {
+
+    public void shutdown(boolean free) {
+
+    }
+}
