a7a8f92c6ccc353e5f9f991987cbd0faaa62f136
Asourcious <barronpm@gmail.com>
2016-11-13 14:51:53 -0500

    Cleaned up PermissionLevel class

--- src/main/java/org/asourcious/plusbot/commands/PermissionLevel.java
+++ src/main/java/org/asourcious/plusbot/commands/PermissionLevel.java
@@ -2,28 +2,29 @@ package org.asourcious.plusbot.commands;
 
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
-import net.dv8tion.jda.core.utils.PermissionUtil;
 import org.asourcious.plusbot.Constants;
 
 public enum PermissionLevel {
 
-    EVERYONE(0),
-    SERVER_MODERATOR(1),
-    SERVER_OWNER(2),
-    OWNER(3);
+    EVERYONE(0, "Everyone"),
+    SERVER_MODERATOR(1, "Server Moderator"),
+    SERVER_OWNER(2, "Server Owner"),
+    OWNER(3, "Bot Owner");
 
     private final int value;
+    private final String name;
 
-    PermissionLevel(int value) {
+    PermissionLevel(int value, String name) {
         this.value = value;
+        this.name = name;
     }
 
     public static PermissionLevel getPermissionLevel(Member member) {
         if (member.getUser().getId().equals(Constants.OWNER_ID))
             return PermissionLevel.OWNER;
-        if (member.getGuild().getOwner().equals(member))
+        if (member.isOwner())
             return PermissionLevel.SERVER_OWNER;
-        if (PermissionUtil.checkPermission(member.getGuild(), member, Permission.MANAGE_SERVER))
+        if (member.hasPermission(Permission.MANAGE_SERVER))
             return PermissionLevel.SERVER_MODERATOR;
 
         return PermissionLevel.EVERYONE;
@@ -39,17 +40,6 @@ public enum PermissionLevel {
 
     @Override
     public String toString() {
-        switch (this) {
-            case EVERYONE:
-                return "Everyone";
-            case SERVER_MODERATOR:
-                return "Moderator";
-            case SERVER_OWNER:
-                return "Owner";
-            case OWNER:
-                return "Bot Owner";
-            default:
-                return "UNKNOWN";
-        }
+        return name;
     }
 }
