fcfe695155814724862d0e830caa80b130a362ab
Asourcious <barronpm@gmail.com>
2016-12-01 21:25:11 -0500

    Added support for JagTags

--- src/main/java/org/asourcious/plusbot/handle/TagHandler.java
+++ src/main/java/org/asourcious/plusbot/handle/TagHandler.java
@@ -1,5 +1,8 @@
 package org.asourcious.plusbot.handle;
 
+import me.jagrosh.jagtag.JagTag;
+import me.jagrosh.jagtag.Method;
+import me.jagrosh.jagtag.Parser;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
@@ -8,12 +11,16 @@ import org.asourcious.plusbot.PlusBot;
 import org.asourcious.plusbot.config.source.GuildTags;
 import org.asourcious.plusbot.util.DiscordUtils;
 
+import java.util.Random;
+
 public class TagHandler {
 
     private PlusBot plusBot;
+    private Random random;
 
     public TagHandler(PlusBot plusBot) {
         this.plusBot = plusBot;
+        this.random = new Random();
     }
 
     public void handleMessage(Message message, User author, TextChannel channel, Guild guild) {
@@ -25,8 +32,11 @@ public class TagHandler {
 
         if (tags.existsWithName(guild.getId(), stripped)) {
             String response = tags.getByName(guild.getId(), stripped);
+            Parser parser = JagTag.newDefaultBuilder()
+                    .addMethod(new Method("random", (env,in) -> in[random.nextInt(in.length)], new String[0]))
+                    .build();
 
-            channel.sendMessage(response).queue();
+            channel.sendMessage(parser.parse(response)).queue();
         }
     }
 }
